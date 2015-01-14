c1d3b2578623a603182391552d6731d387ec6a0f
Siddharth Seth <sseth@apache.org>
2015-01-14 12:25:35 -0800

    Added very simple Scheduler which would work on a single node, and always allocates the requested container.

--- tez-daemon/src/main/java/org/apache/tez/daemon/TezDaemonConfiguration.java
+++ tez-daemon/src/main/java/org/apache/tez/daemon/TezDaemonConfiguration.java
@@ -18,6 +18,10 @@ import org.apache.hadoop.conf.Configuration;
 
 public class TezDaemonConfiguration extends Configuration {
 
+  public TezDaemonConfiguration(Configuration conf) {
+    super(conf);
+    addResource(TEZ_DAEMON_SITE);
+  }
 
   public TezDaemonConfiguration() {
     super(false);
@@ -26,11 +30,10 @@ public class TezDaemonConfiguration extends Configuration {
 
 
   private static final String TEZ_DAEMON_PREFIX = "tez.daemon.";
+  private static final String TEZ_DAEMON_AM_PREFIX = TEZ_DAEMON_PREFIX + "am.";
   private static final String TEZ_DAEMON_SITE = "tez-daemon-site.xml";
 
 
-  public static final String TEZ_DAEMON_NUM_EXECUTORS = TEZ_DAEMON_PREFIX + "num.executors";
-  public static final int TEZ_DAEMON_NUM_EXECUTORS_DEFAULT = 3;
 
   public static final String TEZ_DAEMON_HOSTNAME = TEZ_DAEMON_PREFIX + "hostname";
 
@@ -44,5 +47,19 @@ public class TezDaemonConfiguration extends Configuration {
 
   public static final String TEZ_DAEMON_YARN_SHUFFLE_PORT = TEZ_DAEMON_PREFIX + "yarn.shuffle.port";
 
+
+  // Section for configs used in AM and executors
+  public static final String TEZ_DAEMON_NUM_EXECUTORS = TEZ_DAEMON_PREFIX + "num.executors";
+  public static final int TEZ_DAEMON_NUM_EXECUTORS_DEFAULT = 3;
+
+
+  // Section for configs used in the AM //
+
+  public static final String TEZ_DAEMON_AM_MEMORY_PER_INSTANCE_MB = TEZ_DAEMON_AM_PREFIX + "memory.per.instance.mb";
+  public static final int TEZ_DAEMON_AM_MEMORY_PER_INSTANCE_MB_DEFAULT = 4096;
+  public static final String TEZ_DAEMON_AM_VCPUS_PER_INSTANCE = TEZ_DAEMON_AM_PREFIX + "vcpus.per.instance";
+  public static final int TEZ_DAEMON_AM_VCPUS_PER_INSTANCE_DEFAULT = 4;
+  public static final String TEZ_DAEMON_AM_SERVICE_HOSTS = TEZ_DAEMON_AM_PREFIX + "service.hosts";
+  public static final String TEZ_DAEMON_AM_SERVICE_HOSTS_DEFAULT = "localhost";
 
 }
